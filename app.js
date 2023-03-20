ecaa25841953b957b595aa619fb3a3e29b68446e
Devilbat75 <112400858+Devilbat75@users.noreply.github.com>
2023-03-20 18:24:45 +0100

    création d'utilisateur

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-const Thing = require('./models/thing');
+const userRoutes = require('./routes/user');
 
-app.use(express.json());
+mongoose.connect('mongodb+srv://ludovicbdt:xDIpF4xzpq3JwYuB@cluster0.a1wvpjy.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -12,44 +19,8 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/sauces', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    })
-    thing.save()
-    .then(() => res.status(201).json({ message : 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use(express.json());
 
-app.get('/api/sauces', (req, res, next) => {
-    const sauces = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    res.status(200).json(sauces);
-});
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
-
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://ludovicbdt:xDIpF4xzpq3JwYuB@cluster0.a1wvpjy.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
