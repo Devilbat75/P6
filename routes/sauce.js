ba85637c9a65211d68fa9a65ca77ba512f3f2954
Devilbat75 <112400858+Devilbat75@users.noreply.github.com>
2023-03-23 16:53:29 +0100

    création des sauces

--- routes/sauce.js
+++ routes/sauce.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const auth = require("../middleware/auth")
 
-const sauceCtrl = require('../controllers/sauce')
+const sauceCtrl = require('../controllers/sauce');
+const multer = require('multer');
+
+router.get('/', auth, sauceCtrl.getAllSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put("/:id", auth, multer, sauceCtrl.updateSauce)
+router.delete("/:id", auth, sauceCtrl.deleteSauce)
+//router.post("/:id/like", auth, sauceCtrl.likeDislikeSauce)
 
 module.exports = router;
