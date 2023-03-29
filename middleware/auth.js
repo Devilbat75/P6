679e3c88ae1e25e3acd3da9c25d944e4bc76dc4c
Devilbat75 <112400858+Devilbat75@users.noreply.github.com>
2023-03-29 18:10:45 +0200

    sauce

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,14 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
-
 
 module.exports = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
-    const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
-    } else {
-      next();
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+        req.auth = {
+            userId: userId
+        }; next();
+    } catch(error) {
+        res.status(401).json({ error });
     }
-  } catch {
-    res.status(401).json({
-      error: new Error('Invalid request!')
-    });
-  }
 };
