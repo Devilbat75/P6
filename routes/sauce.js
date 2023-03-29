75388e2174a7489be9d45a96687c4087d3d410e2
Devilbat75 <112400858+Devilbat75@users.noreply.github.com>
2023-03-29 18:00:18 +0200

    sauce maj

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,15 +1,18 @@
+//import express + installation router + url vers les routes dans fichiers controllers
 const express = require('express');
 const router = express.Router();
-const auth = require("../middleware/auth")
+const sauceCtrl = require('../controllers/sauce');
 
-const sauceCtrl = require("../controllers/sauce");
-const multer = require('multer');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.get('/', auth, sauceCtrl.getAllSauces);
-router.get('/:id', auth, sauceCtrl.getOneSauce);
+//creation de nos routes et lien avec le controller correspondant + ajout de "auth" et de "multer" 
 router.post('/', auth, multer, sauceCtrl.createSauce);
-router.put("/:id", auth, multer, sauceCtrl.modifySauce);
-router.delete("/:id", auth, sauceCtrl.deleteSauce);
-router.post("/:id/like", auth, sauceCtrl.likeSauce);
+router.get('/', sauceCtrl.getAllSauces);
+router.put('/:id', auth, multer, sauceCtrl.updateSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
 
+//Export du router
 module.exports = router;
